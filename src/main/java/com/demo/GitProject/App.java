749b805ecb696de4d69e3d689dcea271087a8220
Yamuna45 <kuruvayamuna48@gmail.com>
2025-02-03 10:03:22 +0530

    third commit by 2nd developer

--- src/main/java/com/demo/GitProject/App.java
+++ src/main/java/com/demo/GitProject/App.java
@@ -4,8 +4,12 @@ package com.demo.GitProject;
  * Hello world!
  *
  */
+
 public class App 
 {
+	public void display() {
+		System.out.println("welcome");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
